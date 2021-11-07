8f221f381e44b9d3d5a5a36edd07728d6d7f68da
Haruki <nechaevatany@gmail.com>
2021-11-07 18:54:43 +0400

    SecondHomework: дополнено

--- src/main/java/Homework/SecondHomework.java
+++ src/main/java/Homework/SecondHomework.java
@@ -9,9 +9,38 @@ public class SecondHomework {
         return 2 * pi * Math.sqrt(l / g);
     }
 
+    public static long sum(int a, int b) {
+        return a+b;
+    }
+
+    public static long minus(int c, int d) {
+        return c-d;
+    }
+
+    public static long multiplication(int r, int t) {
+        return r*t;
+    }
+
+    public static double division(int r, int t) {
+        return r/t;
+    }
+
     public static void main(String[] args) {
         double periodResult = period(3.14, 3, 9.81);
         System.out.println(periodResult);
+
+        long summa = sum(4, 4);
+        System.out.println(summa);
+
+        long min = minus(13, 7);
+        System.out.println(min);
+
+        long multip= multiplication(5, 5);
+        System.out.println( multip);
+
+        double div = division(42, 7);
+        System.out.println( div);
+
     }
 }
 
