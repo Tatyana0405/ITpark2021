3fcdddb6ff32b47318af0aae9412c1468bfab00c
Haruki <nechaevatany@gmail.com>
2021-11-12 22:56:15 +0400

    подправлено д/з №4

--- src/main/java/Homework/OldScoolMathSqrt.java
+++ src/main/java/Homework/OldScoolMathSqrt.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class OldScoolMathSqrt {
     public static void main(String[] args) {
         Scanner fact = new Scanner(System.in);
-        System.out.println("Введите значение числа!");
+        System.out.println("Введите значение числа из которого излекается корень");
         int n = fact.nextInt();
         System.out.println("извлекаем корень из числа: " + n);
         int sum = 0, result = 0, i;
@@ -15,7 +15,7 @@ public class OldScoolMathSqrt {
             for (i = 1; result <= n; i += 2, ++sum) {
                 result = result + i;
             }
-            System.out.println("Значение факториала =  " + (sum-1));
+            System.out.println("Значение квадратного корня  =  " + (sum-1));
         }
     }
 }
