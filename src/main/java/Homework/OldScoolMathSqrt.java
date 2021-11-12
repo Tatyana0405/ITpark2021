84d067188cc90e20dabe91b6bd825702ebebb7b7
Haruki <nechaevatany@gmail.com>
2021-11-12 18:43:53 +0400

    lesson4 и д/з MathSqrt добавлены

---
+++ src/main/java/Homework/OldScoolMathSqrt.java
@@ -0,0 +1,21 @@
+package Homework;
+import java.util.Scanner;
+public class OldScoolMathSqrt {
+    public static void main(String[] args) {
+        Scanner fact = new Scanner(System.in);
+        System.out.println("Введите значение числа!");
+        int n = fact.nextInt();
+        System.out.println("извлекаем корень из числа: " + n);
+        int sum = 0, result = 0, i;
+
+
+        if (n < 0) {
+            System.out.println("Число не может быть отрицательным!");
+        } else {
+            for (i = 1; result <= n; i += 2, ++sum) {
+                result = result + i;
+            }
+            System.out.println("Значение факториала =  " + (sum-1));
+        }
+    }
+}
