48dd58b08a4060f677545ab71695bd2239a68464
Haruki <nechaevatany@gmail.com>
2021-11-28 01:34:56 +0400

    разбор дз

--- src/main/java/Homework/Homework6Part2.java
+++ src/main/java/Homework/Homework6Part2.java
@@ -1,47 +1,72 @@
 package Homework;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Homework6Part2 {
 
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        System.out.println("Здравствуйте! Введите количество элементов массива");
-        while (!num.hasNextInt()) {
-            System.out.println("Вводите число, пожалуйста");
-            num.next();
-        }
-        int size = num.nextInt();
-        System.out.println("Количество чисел: " + size);
-        if (size < 0) {
-            System.out.println("Число не может быть отрицательным!");
-        } else {
-            int[] userArray = new int[size];
-            int[] fibonArray = new int[25];
-            fibonArray[0] = 0;
-            fibonArray[1] = 1;
-            fibonArray[2] = 1;
-
-            for (int i = 3; i < fibonArray.length; i++) {
-                fibonArray[i] = fibonArray[i - 2] + fibonArray[i - 1];
+        System.out.println("Здравствуйте! ");
+        int arrayLength = 50;
+        long[] fibonacciArray = new long[arrayLength];
+        int lastIndex = 2;
+        fibonacciArray[0] = 0;
+        fibonacciArray[1] = 1;
+        fibonacciArray[2] = 1;
+        long result;
+        while (true) {
+            int index = getFibonacciIndex(arrayLength);
+            System.out.println(index);
+            if (fibonacciArray[index] == 0) {
+                fillFibonacciArray(fibonacciArray, lastIndex, index);
             }
-
-            if (userArray.length <= fibonArray.length) {
-                System.arraycopy(fibonArray, 0, userArray, 0, userArray.length);
-                System.out.println(Arrays.toString(userArray));
-            } else {
-                System.arraycopy(fibonArray, 0, userArray, 0, fibonArray.length - 3);
-                userArray[fibonArray.length - 2] = fibonArray[fibonArray.length - 2];
-                userArray[fibonArray.length - 1] = fibonArray[fibonArray.length - 1];
-                for (int i = fibonArray.length-1; i < userArray.length; i++) {
-                    userArray[i] = userArray[i - 1] + userArray[i - 2];
-                }
-                System.out.println(Arrays.toString(userArray));
-            }
+            result = fibonacciArray[index];
+            System.out.printf("Число, стоящее на %d индексе имеет значение %d \n", index, result);
         }
     }
+
+    private static long fillFibonacciArray(long[] fibonacciArray, int lastIndex, int index) {
+        long result;
+        System.out.printf("Значение с индексом %d было рассчитано и размещено в кеше\n", index);
+        for (int i = lastIndex; i <= index; i++) {
+            fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+        }
+        result = fibonacciArray[index];
+        return result;
+    }
+
+    private static int getFibonacciIndex(int maxIndex) {
+        System.out.println("Введите позицию в ряде Фибоначчи как целое число");
+        Scanner index = new Scanner(System.in);
+        while (!index.hasNextInt()) {
+            String command = index.next();
+            if ("exit".equalsIgnoreCase(command)) {
+                System.out.println("Спасибо заучастие в программе");
+                System.exit(0);
+            }
+            System.out.println("Вводите число, пожалуйста");
+            index.next();
+        }
+        int number = index.nextInt();
+        if (number <= 0) {
+            System.out.printf("Вы указали отрицательное значение %d, что не допустимо в программе\n", number);
+            return getFibonacciIndex(maxIndex);
+        } else if (number >= maxIndex) {
+            System.out.printf("Вы указали значение большее, чем %d, что не допустимо в программе\n", maxIndex);
+            return getFibonacciIndex(maxIndex);
+        }
+        return number;
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
