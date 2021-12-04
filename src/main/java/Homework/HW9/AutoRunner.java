9f049803700de66e999f431066badf699bda484a
Haruki <nechaevatany@gmail.com>
2021-12-04 21:19:48 +0400

    HW9 подкорректирована

--- src/main/java/Homework/HW9/AutoRunner.java
+++ src/main/java/Homework/HW9/AutoRunner.java
@@ -3,25 +3,20 @@ package Homework.HW9;
 public class AutoRunner {
 
     public static void main(String[] args) {
-        Car[] car =new Car [5];
-        car [0] = new Car("LadaGranta", 500);
-        car[1] = new Car("Priora", 500);
-        car[2]  = new Car("X-Ray", 500);
-        car[3] = new Car("Vesta", 500);
-        car [4] = new Car("2110", 500);
+        Automobile[] automobiles = new Automobile[10];
+        automobiles[0] = new Car("LadaGranta", 500);
+        automobiles[1] = new Car("Priora", 500);
+        automobiles[2] = new Car("X-Ray", 500);
+        automobiles[3] = new Car("Vesta", 500);
+        automobiles[4] = new Car("2110", 500);
+        automobiles[5] = new Truck("Белаз", 1200);
+        automobiles[6] = new Truck("МАЗ", 1200);
+        automobiles[7] = new Truck("Газель", 1200);
+        automobiles[8] = new Truck("ЗИЛ", 1200);
+        automobiles[9] = new Truck("MAN", 1200);
 
-        for (int i=0;i< car.length;i++){
-            car[i].move((int)(Math.random()*1000));
-        }
-        Truck[] truck =new Truck [5];
-        truck [0]= new Truck("Белаз", 1200);
-        truck [1] = new Truck("МАЗ", 1200);
-        truck [2] = new Truck("Газель", 1200);
-        truck [3] = new Truck("ЗИЛ", 1200);
-        truck [4] = new Truck("MAN", 1200);
-
-        for (int i=0;i< car.length;i++){
-            truck[i].move((int)(Math.random()*1000));
+        for (int i = 0; i < automobiles.length; i++) {
+            automobiles[i].move((int) (Math.random() * 1000));
         }
     }
 }
