da5a6f66bbe290d101b47dbe728314d8a2c94efa
Haruki <nechaevatany@gmail.com>
2021-12-14 22:28:36 +0400

    HW11

---
+++ src/main/java/Homework/HW11/Truck.java
@@ -0,0 +1,8 @@
+package Homework.HW11;
+
+public final class Truck extends Auto {
+
+    public Truck(String number, int speed, double weight, double height, double width, double length) {
+        super(number, speed, weight, height, width, length);
+    }
+}
